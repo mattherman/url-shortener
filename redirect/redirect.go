eeff95f49ddc9cabe1eea9ac7c071dd8b2662e55
Matthew Herman <mattherman11@gmail.com>
2017-03-24 11:07:34 -0500

    Read alias from URL

--- redirect/redirect.go
+++ redirect/redirect.go
@@ -1,8 +1,18 @@
 package redirect
 
-import "net/http"
+import (
+	"html"
+	"net/http"
+)
+
+var urls = map[string]string{
+	"ggl": "http://www.google.com",
+	"so":  "http://www.stackoverflow.com",
+	"ds":  "http://www.directsupply.com",
+}
 
 // Redirect will redirect the client based on the shortened URL provided
 func Redirect(w http.ResponseWriter, r *http.Request) {
-	http.Redirect(w, r, "http://www.google.com", 303)
+	path := html.EscapeString(r.URL.Path[1:])
+	http.Redirect(w, r, urls[path], 303)
 }
